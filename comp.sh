7011dc3f2616d45e22c1aad021e896a089f5937f
Luis Enrique <quiquepuma23@gmail.com>
2015-01-21 12:46:50 -0600

    Compilar y correr

---
+++ comp.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  comp.sh
+#  
+#
+#  Created by Luis Enrique Quintanar Cortés on 14/01/15.
+#
+
+g++ -O3 simulacion.cpp -o simulacion
---
+++ sim.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#  sim.sh
+#  
+#
+#  Created by Luis Enrique Quintanar Cortés on 13/01/15.
+#
+#!/bin/bash
+./simulacion 2 2 2 10 2
+
+# primer argumento: tipo de red: 1-cuad 2-triang
+# Segundo argumento: Tipo de modelo: 1-Ising    2-Antiferro
+# Tercer argumento: Tipo de algoritmo: 1-Metrop 2-WL1D  3-WL2D
+# Cuarto argumento: Longitud de la red por lado
+# Quinto argumento: Campo externo
